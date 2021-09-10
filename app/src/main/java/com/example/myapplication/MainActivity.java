84528a2ff248a8106e05f60a26cb124d961d3b26
ejang1 <ejang1@binghamton.edu>
2021-09-10 13:36:01 -0400

    random animal button finish

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //new DownloadImageFromInternet((ImageView) findViewById(R.id.imageView)).execute("https://pbs.twimg.com/profile_images/630285593268752384/iD1MkFQ0.png");
-        new DownloadImageTask((ImageView)findViewById(R.id.imageView)).execute("https://cdn.mos.cms.futurecdn.net/65xDQxBV26qkgupGHbJSSZ-1200-80.jpg");
+        new DownloadImageTask((ImageView)findViewById(R.id.imageView)).execute("https://static.vecteezy.com/system/resources/thumbnails/002/163/795/small/simple-set-of-animal-related-line-icons-free-vector.jpg");
     }
 
     public void animalChange(View view){
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         Object animalName = as.animalSet.keySet().toArray()[randNum];
         TextView animalNameTag = (TextView)findViewById(R.id.animalName);
         animalNameTag.setText((String)animalName);
-        new DownloadImageTask((ImageView)findViewById(R.id.imageView)).execute("https://images.immediate.co.uk/production/volatile/sites/23/2014/10/GettyImages-521375354-a04d423.jpg?quality=90&resize=768%2C574");
+        new DownloadImageTask((ImageView)findViewById(R.id.imageView)).execute(as.animalSet.get((String)animalName));
 
     }
 }
